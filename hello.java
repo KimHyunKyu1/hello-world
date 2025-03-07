597fb98032853113ed80f771341c301972f556e3
gunsu <gunsu980@gmail.com>
2025-03-07 13:35:16 +0900

    zzzzz

--- hello.java
+++ hello.java
@@ -2,6 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         int a = 10;
-        int b = 888888;
+        int b = 8888;
     }
 }
